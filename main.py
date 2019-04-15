0639dd39dab137d855532a755cf1c1821483fac0
ckinsey9 <48170234+ckinsey9@users.noreply.github.com>
2019-04-15 10:59:19 -0500

    added hashing / salt to blogz

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from hashutils import make_pw_hash, check_pw_hash
+#imported the necessary functions for password hashing
 
 
 app = Flask(__name__)
@@ -12,15 +14,15 @@ app.secret_key = "mysecretkey"
 
 
 class User(db.Model):
-
+#added hashing/salting technique to db here
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(50))
+    pw_hash = db.Column(db.String(200))
     blogs = db.relationship("Blog", backref = "author")
 
     def __init__(self, username, password):
         self.username = username
-        self.password = password
+        self.pw_hash = make_pw_hash(password)
 
 class Blog(db.Model):
 
@@ -56,10 +58,10 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
         user = User.query.filter_by(username=username).first()
-        if user and user.password == password:
+        if user and check_pw_hash(password, user.pw_hash):
             session["username"] = username
             return redirect('/newpost')
-        elif user and user.password != password:
+        elif user and not check_pw_hash(password, user.pw_hash):
             flash("Incorrect password, please try again.", "error")
         else:
             flash("Username does not exist. Please try again or click the 'Create Adventures' button to Sign Up.", "error")
